77c49a000b3dd75268bf466b0c3a38e75a4506ec
AdrianGjerstad <48812634+AdrianGjerstad@users.noreply.github.com>
2020-12-15 16:45:38 -0500

    Begin library structure

---
+++ atkcc/atkcc.h
@@ -0,0 +1,16 @@
+#ifndef ATKCC_INCLUDE_ATKCC_H_
+#define ATKCC_INCLUDE_ATKCC_H_
+
+#ifndef ATKCC_VERSION
+  #define ATKCC_VERSION "0.1.0-Unstable"
+  #define ATKCC_MAJOR 0
+  #define ATKCC_MINOR 1
+  #define ATKCC_PATCH 0
+
+  #define ATKCC_UNSTABLE true
+#endif
+
+// Include ALL library files below
+
+#endif  // ATKCC_INCLUDE_ATKCC_H_
+
